f587cd5badd7ee9d2e46d3280e653bb970b6244d
AshishWO <ashishpa.webosmotic@gmail.com>
2025-02-13 11:27:26 +0530

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
         <Script
           src="https://staging-widget.guidy.net/v1/assets/static-pages/injector.js"
           type="module"
-          data-account="m72wzsr35r"
+          data-account="m71j5is6hr"
         ></Script>
       </body>
     </html>

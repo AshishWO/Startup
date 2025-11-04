3d48351085a4f8c961474e3811429329ac5465a3
WOdeveloper <dev.webosmotic@gmail.com>
2025-11-04 13:27:27 +0530

    names added

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -30,6 +30,7 @@ const Contact = () => {
                       <input
                         type="text"
                         id="name"
+                        name="name"
                         placeholder="Enter your name"
                         className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
                       />
@@ -46,6 +47,7 @@ const Contact = () => {
                       <input
                         type="email"
                         id="email"
+                        name="email"
                         placeholder="Enter your email"
                         className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
                       />

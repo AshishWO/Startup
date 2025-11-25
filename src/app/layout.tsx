f3f7f998d6cc4ba452902493343e40672f5c5289
WOdeveloper <dev.webosmotic@gmail.com>
2025-11-25 17:02:22 +0530

    updated the braoza

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
           <ScrollToTop />
         </Providers>
         <Script
-          src="https://staging-widget.guidy.net/v1/assets/static-pages/injector.js"
+          src="https://staging-widget.braoza.com/v1/assets/static-pages/injector.js"
           type="module"
           data-account="mct0cu52og"
         ></Script>

cf0d23e5e9f7da40c126f1fd93bf6328a4b3e3b3
AshishWO <166097527+AshishWO@users.noreply.github.com>
2026-08-03 16:16:29 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
         <Script
           src="https://staging-widget.braoza.com/v1/assets/static-pages/injector.js"
           type="module"
-          data-account="mct0cu52og"
+          data-account="mct0cu52o"
         ></Script>
         {/* <Script
           src="http://127.0.0.1:5500/test-live/injector.js"
